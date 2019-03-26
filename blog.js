c8b4986143b547b54641b0c22e83dbf85032d5f0
ric_z <forveer2013@gmail.com>
2019-03-26 14:57:38 +0800

    today

--- blog.js
+++ blog.js
@@ -1,7 +1,7 @@
 let addJs = (src, async, cb) => {
     let j = document.createElement("script");
     j.src = src;
-    j.async = async ? true : false;
+    j.async = !!async;
     j.onload = j.onreadystatechange = () => {
         if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
             console.log(`load: ${src}`);
@@ -27,16 +27,10 @@ let addNode = (tag, css, html, parentNode) => {
 };
 
 addCss("/style.css");
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1276688436'%3E%3C/span%3E%3Cscript src='https://s23.cnzz.com/z_stat.php%3Fid%3D1276688436%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
-document.write(`<p style="margin: 35px">转载请注明原文地址： <a href='${location.href}'>${location.href}</a>　<small>(${(document.head.querySelector('[name=modifydate]') | {content:"--"}).content})</small></p>`);
 
 window.addEventListener("load", () => {
     let head = addNode('nav', "top: 0;left: 0;width: 100%;height: 40px;padding: 0 20px;line-height: 40px;font-size:18px;white-space: nowrap;text-overflow: hidden;text-overflow: ellipsis;overflow: hidden;", `<a href='/index.html'>翻阅其它日志</a>　<div class='text-loop' style='display: inline-block;'>${document.title}</div>`, document.body);
     head.setAttribute("title", document.title);
-
-    // 评论
-    addNode('div', "", `<h2>留言</h2><div id="lv-container" data-id="city" data-uid="MTAyMC80MzM4Ny8xOTkyOA==">`, document.body);
-    addJs("https://cdn-city.livere.com/js/embed.dist.js", true);
 
     // 转换 markdown
     addCss("/marked.css");
@@ -51,7 +45,6 @@ window.addEventListener("load", () => {
                 }
             });
             txt.parentElement.replaceChild(md, txt);
-            addJs("/nav.js", true);
         });
     });
-});
+}, true);
