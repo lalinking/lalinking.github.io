59d3a635ce5bf1d9e334cd5fa4ebcdebcfc5990b
ric_z <forveer2013@gmail.com>
2019-04-01 15:16:27 +0800

    增加加载特效

--- blog.js
+++ blog.js
@@ -1,4 +1,3 @@
-document.write(unescape("%3Cspan id='cnzz_stat_icon_1276688436'%3E%3C/span%3E%3Cscript src='https://s23.cnzz.com/z_stat.php%3Fid%3D1276688436%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
 let addJs = (src, async, cb) => {
     let j = document.createElement("script");
     j.src = src;
@@ -17,6 +16,12 @@ let addCss = (src) => {
     j.href = src;
     j.type = "text/css";
     j.rel = "stylesheet";
+    j.async = true;
+    j.onload = j.onreadystatechange = () => {
+        if (!this.readyState || this.readyState === "loaded" || this.readyState === "complete") {
+            console.log(`load: ${src}`);
+        }
+    };
     document.getElementsByTagName("head")[0].appendChild(j);
 };
 let addNode = (tag, css, html) => {
@@ -27,8 +32,9 @@ let addNode = (tag, css, html) => {
     return nod;
 };
 
+document.write("<div style='width: 100%;height: 100%;position: fixed;top: 0;left: 0;z-index: 10000;background: white;' id='msg-panel'><div class='spinner'></div></div>");
+document.write(unescape("%3Cspan id='cnzz_stat_icon_1276688436'%3E%3C/span%3E%3Cscript src='https://s23.cnzz.com/z_stat.php%3Fid%3D1276688436%26show%3Dpic' type='text/javascript'%3E%3C/script%3E"));
 addCss("/style.css");
-
 window.addEventListener("load", () => {
     // 转换 markdown
     addCss("/marked.css");
@@ -44,6 +50,7 @@ window.addEventListener("load", () => {
             });
             txt.parentElement.replaceChild(md, txt);
             window.onmdload && window.onmdload();
+            document.body.removeChild(document.getElementById("msg-panel"))
         });
     });
 }, true);
