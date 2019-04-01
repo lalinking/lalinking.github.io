1908943bffffbdb80f967b02f3886d990843a9c8
ric_z <forveer2013@gmail.com>
2019-04-01 11:42:50 +0800

    调整导航滚动位置

--- nav.js
+++ nav.js
@@ -4,10 +4,10 @@ function setNav() {
     let txt = '';
     let hs = document.querySelectorAll("h1,h2,h3,h4,h5,h6");
     pos.length = 0;
-    hs.forEach((h, i) => {
+    hs.forEach((h) => {
         let _c = h.innerText.replace(/^(((\w\w)|(\w)|([^\x00-\xff])){2}).*$/, "$1");
-        txt += `<div><a href='javascript:window.scrollTo(0,${h.offsetTop - 8})' title='${h.innerText}'>${h.innerText === _c ? _c : (_c + '..')}</a></div>`;
-        pos.push(h.offsetTop - 8);
+        txt += `<div><a href='javascript:window.scrollTo(0,${h.offsetTop - 40})' title='${h.innerText}'>${h.innerText === _c ? _c : (_c + '..')}</a></div>`;
+        pos.push(h.offsetTop - 40);
     });
     nav.innerHTML = txt;
     nav.style = `bottom: calc(50% - ${hs.length*20+6}px);right: 20px;z-index: 1314;width: 65px;padding: 6px;line-height: 40px;`;
@@ -17,7 +17,7 @@ let scrollfun = () => {
     pos.forEach((po,indx) => {
         let prt = anchors[indx].parentElement;
         prt.style = "";
-        if (po < window.pageYOffset+8) {
+        if (po < window.pageYOffset + 40 {
             prt.style = "border-left: solid #b96598 3px;padding-left: 3px;";
         }
     });
