c6ce15faebad4f6254e1b50fbfb9902b441395c3
ric_z <forveer2013@gmail.com>
2019-04-23 19:31:25 +0800

    更新样式

--- blog.js
+++ blog.js
@@ -43,15 +43,23 @@ const funDownload = function (content, filename) {
     document.body.removeChild(eleLink);
 };
 
-function copyToClipboard(text,cb,errcb) {
+function copyToClipboard(text, cb, errcb) {
     if (!navigator.clipboard) {
         errcb("copy fail.");
         return;
     }
-    navigator.clipboard.writeText(text).then(cb,errcb);
+    navigator.clipboard.writeText(text).then(cb, errcb);
 }
 
-let p = location.search.length ? location.search.substring(3) : "index.txt";
+let search = ((function () {
+    let URLParams = {p: "File List", t: "index.txt"};
+    let aParams = decodeURI(document.location.search).substr(1).split('&');
+    for (i = 0; i < aParams.length; i++) {
+        let aParam = aParams[i].split('=');
+        URLParams[aParam[0]] = aParam[1];
+    }
+    return URLParams;
+})());
 /**
  * 引导页的目录
  * @type {string}
@@ -84,41 +92,48 @@ const choiceKeyword = function (k) {
 let indTxt = "";
 let getIndexMD = (conf) => {
     let txt = "";
-    txt += "\n### 文件标签 \n   选中标签进行过滤\n\n  ";
+    txt += "\n### 选中标签筛选内容\n  ";
     let kws = new Set();
-    Object.values(conf).forEach(jp => {
-        jp.keywords && (jp.keywords.split(/\s*,\s*/).forEach(k => {
-            kws.add(k)
-        }))
+    Object.values(conf).forEach(ls => {
+        Object.values(ls).forEach(
+            jp => {
+                jp.keywords && (jp.keywords.split(/\s*,\s*/).forEach(k => {
+                    kws.add(k)
+                }))
+            }
+        )
     });
     kws.forEach((k) => {
         txt += ` [${k}](javascript:choiceKeyword('${encodeURI(k)}')) `;
     });
-    txt += "\n### 文件列表";
-    for (let j in conf) {
-        let jp = conf[j];
-        txt += `\n1. <small>(${jp.modifydate})</small> [${jp.title}](/?p=${j}#${encodeURI(jp.keywords).replace(/,/g,"#")})\n   <small>${jp.description}</small>`;
+    for (let ls in conf) {
+        txt += `\n\n###  ${ls} `;
+        for (let j in conf[ls]) {
+            let jp = conf[ls][j];
+            txt += `\n1. <small>(${jp.modifydate})</small> [${jp.title}](/?p=${encodeURI(ls)}&t=${encodeURI(j)}#${encodeURI(jp.keywords).replace(/,/g, "#")})\n   <small>${jp.description}</small>`;
+        }
     }
     return txt;
 };
 let onGetJson = (json) => {
     let conf = JSON.parse(json);
-    if (p === "index.txt") {
+    if (search.t === "index.txt") {
         // 计算md
         indTxt = getIndexMD(conf);
     } else {
-        document.title = conf[p].title;
-        let head = createNode('nav', "head", `<a href='/' style="vertical-align: top;">翻阅其它文件</a>　<span class='text-title'>${conf[p].title}</span>`);
-        head.setAttribute("title", conf[p].title);
+        let conf_page = conf[search.p][search.t];
+        document.title = conf_page.title;
+        let head = createNode('nav', "head", `<a href='/' style="vertical-align: top;">翻阅其它文件</a>　<span class='text-title'>${conf_page.title}</span>`);
+        head.setAttribute("title", conf_page.title);
         document.body.appendChild(head);
         let leadTxt = "";
-        leadTxt += `<small>&nbsp;&nbsp;创建于:&nbsp;${conf[p].modifydate}&nbsp;&nbsp;阅读量:&nbsp;<span class="leancloud-visitors" data-flag-title="${conf[p].title}" id="${p}"><i class="leancloud-visitors-count">--</i></span></small>`;
+        leadTxt += `<small>&nbsp;&nbsp;创建于:&nbsp;${conf_page.modifydate}&nbsp;&nbsp;阅读量:&nbsp;<span class="leancloud-visitors" data-flag-title="${conf_page.title}" id="${search.t}"><i class="leancloud-visitors-count">--</i></span></small>`;
         leadTxt += "<br/>";
-        leadTxt += `<small>&nbsp;&nbsp;关键词:&nbsp;<span class='keyword'>${conf[p].keywords.split(",").join("</span>&nbsp;&nbsp;<span class='keyword'>")}</span></small>`;
+        leadTxt += `<small>&nbsp;&nbsp;关键词:&nbsp;<span class='keyword'>${conf_page.keywords.split(",").join("</span>&nbsp;&nbsp;<span class='keyword'>")}</span></small>`;
         leadTxt += "<br/>";
-        leadTxt += `<small>&nbsp;&nbsp;　说明:&nbsp;${conf[p].description || "无"}</small>`;
+        leadTxt += `<small>&nbsp;&nbsp;　说明:&nbsp;${conf_page.description || "无"}</small>`;
         leadTxt += "<br/><br/>";
-        leadTxt += `<i>转载请注明原文地址： <a href='${conf[p].href || location.href}'>${conf[p].href || location.href.replace(/#.*/,"")}</a></i>`;
+        leadTxt += `<i>转载请注明原文地址： <a href='${conf_page.href || location.href}'>${conf_page.href || location.href.replace(/#.*/, "")}</a></i>`;
         let cp = createNode("p", "lead-txt", leadTxt);
         document.body.insertBefore(cp, document.body.childNodes[0]);
         addJs("/nav.js", true, window.initNav);
@@ -129,7 +144,7 @@ let onGetJson = (json) => {
     addJs("https://cdn1.lncld.net/static/js/3.0.4/av-min.js", true, () => {
         addJs("https://unpkg.com/valine@1.3.6/dist/Valine.min.js", true, () => {
             new Valine({
-                path: p,
+                path: search.t,
                 el: '#vcomments',
                 appId: 'TtiWfdzc3Pcwy62vcXJj4zKl-gzGzoHsz',
                 appKey: 'UzO9Cq4rVPLwyOKmolTwYAXo',
@@ -158,7 +173,7 @@ let onGetMd = (txt) => {
                                 let expand = "<a onclick='this.parentElement.parentElement.parentElement.parentElement.style.maxHeight=\"none\"; this.parentElement.style.display=\"none\"'>expand</a>";
                                 return `<div><div class="line-body tool-bar">${expand}</div><div class="mermaid">${code}</div></div>`;
                             } else {
-                                let c = lan? Prism.highlight(code, Prism.languages[lan], lan) : code;
+                                let c = lan ? Prism.highlight(code, Prism.languages[lan], lan) : code;
                                 let rs = c.split(/\n/);
                                 let expand = rs.length > 20 ? "<a onclick='this.parentElement.parentElement.parentElement.parentElement.style.maxHeight=\"none\"; this.style.display=\"none\"'>expand</a>" : "";
                                 let download = `<a onclick="let txt = '';this.parentElement.parentElement.parentElement.querySelectorAll('code>div:not(:first-child) .line-body').forEach(e=>{txt+=e.textContent+'\\n'});funDownload(txt, '*.${lan}')">download</a>`;
@@ -184,7 +199,7 @@ let onGetMd = (txt) => {
 };
 window.addEventListener("load", () => {
     getAjax("/index.json", onGetJson);
-    getAjax(p, onGetMd);
+    getAjax(search.t, onGetMd);
 
     function resize() {
         document.body.className = document.body.clientWidth > 800 ? "" : "wap";
