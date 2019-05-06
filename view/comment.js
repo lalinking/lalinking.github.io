80efbf2dec7bf8d3c0d380926cffef0f07571870
ric_z <forveer2013@gmail.com>
2019-05-06 13:51:11 +0800

    新增流程图页面

--- view/comment.js
+++ view/comment.js
@@ -1,6 +1,7 @@
-let count = utils.createNode('span', "hide", `<i class="leancloud-visitors" data-flag-title="${document.title}" id="${utils.search.path}"><i class="leancloud-visitors-count"/></i>`);
-document.body.appendChild(count);
 window.addEventListener("load", () => {
+    let pageid = utils.search.pageid || Object.values(utils.search)[0] || "unknow";
+    let count = utils.createNode('span', "hide", `<i class="leancloud-visitors" data-flag-title="${document.title}" id="${pageid}"><i class="leancloud-visitors-count"/></i>`);
+    document.body.appendChild(count);
     let observer = new MutationObserver(() => {
         let textContent = utils.$(".leancloud-visitors-count", count)[0].textContent;
         textContent && window.ongetvisitcount && ongetvisitcount(textContent)
@@ -9,7 +10,7 @@ window.addEventListener("load", () => {
     utils.addJs("/3rd-lib/av-min.3.0.4.js", true, () => {
         utils.addJs("/3rd-lib/Valine.min.1.3.6.js", true, () => {
             new Valine({
-                path: utils.search.pageid || Object.values(utils.search)[0] || "unknow",
+                path: pageid,
                 el: '#vcomments',
                 appId: 'TtiWfdzc3Pcwy62vcXJj4zKl-gzGzoHsz',
                 appKey: 'UzO9Cq4rVPLwyOKmolTwYAXo',
