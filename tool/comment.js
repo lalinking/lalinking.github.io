a42f4138e22d49044718233829b282a9edb2d388
--add <773002502@qq.com>
2021-08-24 17:18:31 +0800

    升级gitalk

--- tool/comment.js
+++ tool/comment.js
@@ -1,4 +1,4 @@
-addCss("/3rd-lib/gitalk/gitalk.css");
+addCss("https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.css");
 
 let pageKey;
 function initComment(pageid) {
@@ -7,7 +7,7 @@ function initComment(pageid) {
     if (pageKey.length > 50) {
         pageKey = pageKey.substr(pageKey.length - 50)
     }
-    addJs("/3rd-lib/gitalk/gitalk.js", false, () => {
+    addJs("https://cdn.jsdelivr.net/npm/gitalk@1/dist/gitalk.min.js", false, () => {
         new Gitalk({
             clientID: '7b9679434b225da457ea',
             clientSecret: 'd0367170ba5aca675670eb66f6e25689e905bd07',
