d20fe8ec4e0628a92d16a32b45ada6dacccd4533
张明 <forveer2013@gmail.com>
2021-09-27 06:50:06 +0000

    aaa

--- src/fresh.sh
+++ src/fresh.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
 # 获取脚本运行目录
-POSTS_DIR=$(dirname $(readlink -f $0))
-echo $POSTS_DIR
+DIR=$(dirname $(readlink -f $0))
+POSTS_DIR="$DIR/posts/"
+# 拉取博文
+git -C "$POSTS_DIR" pull origin master
+# 生成博文
+node "$DIR/compile.js" "$POSTS_DIR" "$DIR"
