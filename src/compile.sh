59e2bf35bb69e402d873b351a10bb0126fc2cbc8
张明 <forveer2013@gmail.com>
2021-09-28 10:47:38 +0000

    from compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -10,12 +10,6 @@ git -C "$POSTS_DIR" pull origin master
 # 生成博文
 node "$DIR/src/compile.js" "$POSTS_DIR" "$DIR"
 # 推送更新
-git -C "$DIR" add *.md
-git -C "$DIR" add *.html
-git -C "$DIR" add *.js
-git -C "$DIR" add *.css
-git -C "$DIR" add *.png
-git -C "$DIR" add *.ico
-git -C "$DIR" add *.sh
+git -C "$DIR" add *
 git -C "$DIR" commit -m "from compile.sh"
 git -C "$DIR" push
