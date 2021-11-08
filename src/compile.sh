b4378440605f5fc47a05d525bc788ba7a0122dfd
张明 <forveer2013@gmail.com>
2021-11-08 03:21:41 +0000

    from compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -5,17 +5,19 @@ DIR=$(dirname $(readlink -f "$0/../"))
 POSTS_DIR=$(dirname $(readlink -f "$DIR"))/posts/
 echo $POSTS_DIR
 
-git -C "$DIR" pull origin master
 # 拉取博文
 git -C "$POSTS_DIR" pull origin master
+cd $DIR
+# 代码保持最新
+git pull origin master
 # 生成博文
-node "$DIR/src/compile.js" "$POSTS_DIR" "$DIR"
+node "./src/compile.js" "$POSTS_DIR" "$DIR"
 # 推送更新
-git -C "$DIR" add *
-git -C "$DIR" add .posts/
-git -C "$DIR" add 3rd-lib/
-git -C "$DIR" add page/
-git -C "$DIR" add resource/
-git -C "$DIR" add src/
-git -C "$DIR" commit -m "from compile.sh"
-git -C "$DIR" push
+git add *
+git add .posts/
+git add 3rd-lib/
+git add page/
+git add resource/
+git add src/
+git commit -m "from compile.sh"
+git push
