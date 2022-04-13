09d808bafdd06255a2a40b0f418b53877b4d5585
张明 <forveer2013@gmail.com>
2022-04-13 05:57:41 +0000

    from compile.sh

--- src/compile.sh
+++ src/compile.sh
@@ -15,7 +15,7 @@ cd $DIR
 # 代码保持最新
 git pull origin master
 # 生成博文
-node "./src/compile.js" "$POSTS_DIR" "$DIR" "v1"
+node "./src/compile.js" "$POSTS_DIR" "$DIR" ".v1"
 # 推送更新
 git add *
 git add .posts/
