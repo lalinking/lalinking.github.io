693dbf3d61fbb918636cb27fed4cd634a570ed5d
--add <773002502@qq.com>
2022-04-24 12:06:44 +0800

    谷歌垃圾了

--- src/compile.js
+++ src/compile.js
@@ -211,7 +211,11 @@ function loadSiteMap() {
     });
     mapXmlStr += '</urlset>';
     fs.writeFileSync(dirRepo + "/sitemap_1.XML", mapXmlStr);
+    fs.writeFileSync(dirRepo + "/sitemap.XML", mapXmlStr);
+    fs.writeFileSync(dirRepo + "/sitemap.xml", mapXmlStr);
     fs.writeFileSync(dirRepo + "/sitemap_1.TXT", mapTxtStr);
+    fs.writeFileSync(dirRepo + "/sitemap.TXT", mapTxtStr);
+    fs.writeFileSync(dirRepo + "/sitemap.txt", mapTxtStr);
 }
 
 // 获取元数据 & 编译博文
